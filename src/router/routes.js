275f5d9b2d41a78a1384958f7ba402cf6f99408f
Alex Mur <lehamuraschov2014@yandex.ru>
2023-10-07 15:17:48 +0200

    lesson1

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,12 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/MenuLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/profile', component: () => import('pages/profileF.vue') },
+      { path: '/project', component: () => import('pages/projectF.vue') },
+      { path: '/record', component: () => import('pages/recordF.vue') }
     ]
   },
 
