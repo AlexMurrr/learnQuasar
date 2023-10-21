16b4f0596454e149d051bf8511774c5897d9e21e
Alex Mur <lehamuraschov2014@yandex.ru>
2023-10-22 00:03:59 +0200

    rename posts.vue to postsF.vue

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,12 +8,10 @@ const routes = [
       { path: '/profile', component: () => import('pages/profileF.vue') },
       { path: '/project', component: () => import('pages/projectF.vue') },
       { path: '/record', component: () => import('pages/recordF.vue') },
-      { path: '/posts', component: () => import('pages/posts.vue') }
+      { path: '/posts', component: () => import('pages/postsF.vue') }
     ]
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
+  
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
