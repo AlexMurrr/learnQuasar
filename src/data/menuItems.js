81e074525fbac3f96ab6cc470cf35ec4ce43b45b
Alex Mur <lehamuraschov2014@yandex.ru>
2023-10-23 16:58:49 +0200

    add list of posts in postsF.vue

--- src/data/menuItems.js
+++ src/data/menuItems.js
@@ -1,22 +1,22 @@
 export const menuItem = [
-    {
-        displayName: 'Запись',
-        icon: '',
-        link: {path: ''}
-    },
-    {
-        displayName: 'Профиль',
-        icon: '',
-        link: {path: ''}
-    },
-    {
-        displayName: 'Проэкты',
-        icon: '',
-        link: {path: ''}
-    },
-    {
-        displayName: 'Филифлы',
-        icon: '',
-        link: {path: ''}
-    },
-]
+  {
+    displayName: "Запись",
+    icon: "",
+    link: { path: "" },
+  },
+  {
+    displayName: "Профиль",
+    icon: "",
+    link: { path: "" },
+  },
+  {
+    displayName: "Проэкты",
+    icon: "",
+    link: { path: "" },
+  },
+  {
+    displayName: "Филиалы",
+    icon: "",
+    link: { path: "" },
+  },
+];
--- src/pages/postsF.vue
+++ src/pages/postsF.vue
@@ -1,11 +1,31 @@
 <template>
-  <q-page padding>
-   posts
+  <q-page class="flex flex-center">
+    <h4>posts</h4>
+    <q-list bordered separator>
+      <q-item clickable v-ripple v-for="post in posts" :key="post.id">
+        <q-item-section>
+          <q-item-label overline>{{ post.title }}</q-item-label>
+          <q-item-label>{{ post.body }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-  // name: 'PageName',
-}
+  name: "postsF",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {
+    this.$axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((respons) => {
+        this.posts = respons.data;
+      });
+  },
+};
 </script>
