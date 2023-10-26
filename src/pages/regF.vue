b9ad817ff62bed9fc6da78f5cbeb0d11d5489721
Alex Mur <lehamuraschov2014@yandex.ru>
2023-10-26 12:47:30 +0200

    created a registration form using GPT chat

--- src/pages/regF.vue
+++ src/pages/regF.vue
@@ -1,22 +1,65 @@
 <template>
-  <q-page padding>
-    <h5>Регистрация</h5>
-
-    <div class="q-pa-md">
-      <div class="q-gutter-md" style="max-width: 300px">
-        <q-input filled v-model="text" type="email" label="Your Email" />
-
-        <q-input filled v-model="text" type="password" label="Password" />
-      </div>
-    </div>
-  </q-page>
+  <div class="registration-form">
+    <q-page-container>
+      <q-card class="q-ma-md" style="max-width: 400px">
+        <q-card-section>
+          <q-form @submit="submitForm">
+            <q-input
+              outlined
+              v-model="name"
+              label="Имя"
+              class="q-mb-md"
+            ></q-input>
+            <q-input
+              outlined
+              v-model="email"
+              label="Email"
+              type="email"
+              class="q-mb-md"
+            ></q-input>
+            <q-input
+              outlined
+              v-model="password"
+              label="Пароль"
+              type="password"
+              class="q-mb-md"
+            ></q-input>
+            <q-btn
+              color="primary"
+              label="Зарегистрироваться"
+              type="submit"
+            ></q-btn>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </q-page-container>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "regF",
   data() {
-    return { text: "" };
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      // Здесь можно выполнить дополнительные действия, например,
+      //отправить данные на сервер
+      console.log("Форма отправлена");
+    },
   },
 };
 </script>
+
+<style scoped>
+.registration-form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+}
+</style>
