7ec081362bf47e827e1bab02ed20079d78872903
Alex Mur <lehamuraschov2014@yandex.ru>
2023-10-26 12:00:55 +0200

    create form registration without button

--- src/pages/regF.vue
+++ src/pages/regF.vue
@@ -1,9 +1,22 @@
 <template>
-  <q-page padding> регистрация </q-page>
+  <q-page padding>
+    <h5>Регистрация</h5>
+
+    <div class="q-pa-md">
+      <div class="q-gutter-md" style="max-width: 300px">
+        <q-input filled v-model="text" type="email" label="Your Email" />
+
+        <q-input filled v-model="text" type="password" label="Password" />
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
 export default {
   name: "regF",
+  data() {
+    return { text: "" };
+  },
 };
 </script>
